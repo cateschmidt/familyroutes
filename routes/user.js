155a3014056ba8850da5f4591fc77f7feb8cf751
Diarndt <diarndt36d@gmail.com>
2023-06-21 14:31:21 -0700

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -13,4 +13,8 @@ router.get('/:id', usersController.getSingle);
 
 // Ooops, I have no idea how to add a user. I need to research this (Beth)
 
+//Delete User
+router.delete('/:id', usersController.removeUser);
+
+module.exports = router;
 
