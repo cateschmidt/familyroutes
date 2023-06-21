102d175d94ff549bf254fed8e3bd747792105ac0
cateschmidt <c8schmidt@gmail.com>
2023-06-20 20:12:42 -0400

    initial commit

--- routes/dFemales.js
+++ routes/dFemales.js
@@ -7,10 +7,16 @@ const dFemalesController = require('../takeTheWheel/dFemales');
 const validation = require('../middleware/validate');
 
 
+// Get
+///////////////////
+router.get('/', dFemalesController.getAll);
+router.get('/:id', dFemalesController.getSingle);
+// Post
+///////////////////
 router.post('/', validation.saveDfemale, dFemalesController.pushingUpDaisies);
-
+// Delete
+/////////////////
 router.delete('/:id', dFemalesController.pullDaisies);
 
-router.put('/:id',dFemalesController.puttingDaisies);
-//is this extra branch working test
+
 module.exports = router;
