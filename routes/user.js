7f0b7c27e3cc4d5dc57e49cd356f3a5e9ec52aa9
cateschmidt <c8schmidt@gmail.com>
2023-06-20 21:16:02 -0400

    users get all and get single

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,16 @@
 // User Routes
+const express = require('express');
+const router = express.Router();
+
+const lMalesController = require('../takeTheWheel/lMales');
+const validation = require('../middleware/validate');
+
+
+// Get
+////////////////////
+router.get('/', usersController.getAll);
+router.get('/:id', usersController.getSingle);
 
 // Ooops, I have no idea how to add a user. I need to research this (Beth)
-
-
 
 
