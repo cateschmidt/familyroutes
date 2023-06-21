04bf22b694301aebb76435dad66f927409335162
cateschmidt <c8schmidt@gmail.com>
2023-06-20 20:54:17 -0400

    update dmales routes and controller

--- routes/dMales.js
+++ routes/dMales.js
@@ -6,9 +6,15 @@ const router = express.Router();
 const dMalesController = require('../takeTheWheel/dMales');
 const validation = require('../middleware/validate');
 
-
+// Get
+////////////////////
+router.get('/', dMalesController.getAll);
+router.get('/:id', dMalesController.getSingle);
+// Post
+///////////////////
 router.post('/', validation.saveDmale, dMalesController.valhalla);
-
+// Delete
+//////////////////
 router.delete('/:id', dMalesController.removeValhalla);
 
 
